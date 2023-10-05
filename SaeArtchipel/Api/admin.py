aced15c9bb572cf7f5002addb8b74c145fd35376
Bryan Corret <bryan.corret@etu.univ-orleans.fr>
2023-10-05 08:29:26 +0200

    [REBASE] dev->API

--- SaeArtchipel/Api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Lieux
-
-class LieuxAdmin(admin.ModelAdmin):
-    list_display = ('nom', 'description', 'open')
-
-# Register your models here.
-
-admin.site.register(Lieux, LieuxAdmin)
--- SaeArtchipel/api/views.py
+++ SaeArtchipel/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import LieuxSerializer
+from .models import Lieux
 
 # Create your views here.
+
+class LieuxView(viewsets.ModelViewSet):
+    serializer_class = LieuxSerializer
+    queryset = Lieux.objects.all()

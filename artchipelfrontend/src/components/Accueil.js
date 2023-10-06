a33bbea6e7c8667af0dfc7d254fe64501c92176c
FerreiraG <gabriel.ferreira@etu.univ-orleans.fr>
2023-10-06 08:46:21 +0200

    [UPDATE] accueil

--- artchipelfrontend/src/components/Accueil.js
+++ artchipelfrontend/src/components/Accueil.js
@@ -5,31 +5,54 @@ import franceImage from "../components/images/france.png";
 import calendrierImage from "../components/images/calendrier.png";
 import destinationImage from "../components/images/destination.png";
 
+const imageStyle = {
+  width: "5vw", // ajustez la largeur comme vous le souhaitez
+  height: "10vh", // ajustez la hauteur comme vous le souhaitez
+};
+
 const Accueil = () => {
   return (
     <div className="indexAccueil">
-      <h1 className="titreNoir">Art <span className="motVert">Chipel</span></h1>
+      <h1 className="titreNoir display-1 mt-5 mb-5 pt-5 pb-5">Art <span className="motVert">Chipel</span></h1>
       <div className="bandeauVert"></div>
-      <div className="boutons">
-        <div className="carréVert"></div>
-        <button className="boutonRond vert">
-          <img src={lieuImage} alt="Lieu" />
-        </button>
-        <span>Lieu</span>
+      <div className="container mt-3 pt-3">
+        <div className="row justify-content-center align-items-center">
+          <div className="col-sm-2">
+            <div className="d-flex flex-column align-items-center">
+              <button className="btn btn-success btn-sm mx-2">
+                <img src={lieuImage} alt="Lieu" style={imageStyle} />
+              </button>
+              <span>Lieu</span>
+            </div>
+          </div>
 
-        <button className="boutonRond rose">
-          <img src={franceImage} alt="France" />
-        </button>
-        <span>France</span>
-        <button className="boutonRond bleu">
-          <img src={calendrierImage} alt="Calendrier" />
-        </button>
-        <span>Calendrier</span>
-        <button className="boutonRond rouge">
-          <img src={destinationImage} alt="Destination" />
-        </button>
-        <span>Destination</span>
-        <div className="carréVert"></div>
+          <div className="col-sm-2">
+            <div className="d-flex flex-column align-items-center">
+              <button className="btn btn-danger btn-sm mx-2">
+                <img src={franceImage} alt="France" style={imageStyle} />
+              </button>
+              <span>France</span>
+            </div>
+          </div>
+
+          <div className="col-sm-2">
+            <div className="d-flex flex-column align-items-center">
+              <button className="btn btn-primary btn-sm mx-2">
+                <img src={calendrierImage} alt="Calendrier" style={imageStyle} />
+              </button>
+              <span>Calendrier</span>
+            </div>
+          </div>
+
+          <div className="col-sm-2">
+            <div className="d-flex flex-column align-items-center">
+              <button className="btn btn-warning btn-sm mx-2">
+                <img src={destinationImage} alt="Destination" style={imageStyle} />
+              </button>
+              <span>Destination</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );

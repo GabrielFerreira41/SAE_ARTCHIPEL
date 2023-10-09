6ffe92737eeeb2b96ff91d713faa700577be0aad
FerreiraG <gabriel.ferreira@etu.univ-orleans.fr>
2023-10-09 17:43:19 +0200

    FRONT/-IUT-003 Ajout connexion utilisateur sans verification

--- artchipelfrontend/src/components/Header.js
+++ artchipelfrontend/src/components/Header.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Modal, Button, Form } from 'react-bootstrap';
 import '../style/styleAccueill.css';
 
 const Header = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleShowModal = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
   return (
     <header className="bg-white">
       <nav className="navbar navbar-expand-lg navbar-light">
@@ -14,7 +25,7 @@ const Header = () => {
             aria-controls="navbarNav"
             aria-expanded="false"
             aria-label="Toggle navigation"
->
+          >
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
@@ -40,16 +51,16 @@ const Header = () => {
               alt="Line"
               src="https://cdn.animaapp.com/projects/6511c698c032644aea5b6c4d/releases/6511c7272274091aaeadcb28/img/line-6.svg"
             />
-          <img
-            className="img"
-            alt="Line"
-            src="https://cdn.animaapp.com/projects/6511c698c032644aea5b6c4d/releases/6511c7272274091aaeadcb28/img/line-7.svg"
-          />
-          <img
-            className="line-2"
-            alt="Line"
-            src="https://cdn.animaapp.com/projects/6511c698c032644aea5b6c4d/releases/6511c7272274091aaeadcb28/img/line-8.svg"
-          />
+            <img
+              className="img"
+              alt="Line"
+              src="https://cdn.animaapp.com/projects/6511c698c032644aea5b6c4d/releases/6511c7272274091aaeadcb28/img/line-7.svg"
+            />
+            <img
+              className="line-2"
+              alt="Line"
+              src="https://cdn.animaapp.com/projects/6511c698c032644aea5b6c4d/releases/6511c7272274091aaeadcb28/img/line-8.svg"
+            />
             <form className="form-inline ml-auto">
               <input
                 className="form-control"
@@ -60,10 +71,39 @@ const Header = () => {
               <button className="btn btn-outline-primary" type="submit">
                 <i className="fas fa-search"></i>
               </button>
+              <Button variant="primary" className="ml-2" onClick={handleShowModal}>
+                Connexion
+              </Button>
             </form>
           </div>
         </div>
       </nav>
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Formulaire de Connexion</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="username">
+              <Form.Label>Nom d'utilisateur</Form.Label>
+              <Form.Control type="text" placeholder="Entrez votre nom d'utilisateur" />
+            </Form.Group>
+
+            <Form.Group controlId="password">
+              <Form.Label>Mot de passe</Form.Label>
+              <Form.Control type="password" placeholder="Entrez votre mot de passe" />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            Annuler
+          </Button>
+          <Button variant="primary">
+            Se Connecter
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </header>
   );
 };

c4586ded8d9e5a91b698ab845e280dbeab77f0b1
FerreiraG <gabriel.ferreira@etu.univ-orleans.fr>
2023-11-23 15:34:43 +0100

    [ADD] modification page d'accueil

--- artchipelfrontend/src/components/Accueil.js
+++ artchipelfrontend/src/components/Accueil.js
@@ -1,94 +1,48 @@
 import React from "react";
-import "../style/styleAccueill.css";
-import lieuImage from "../components/images/lieu.png";
-import franceImage from "../components/images/france.png";
-import calendrierImage from "../components/images/calendrier.png";
-import destinationImage from "../components/images/destination.png";
-import { Link } from 'react-router-dom';
+import "../style/styleAccueil.css";
+import banniereFestival from "../components/images/banniéreFestival.jpg";
+import parcours from "../components/images/parcours.jpg"
+import carte from "../components/images/carte.jpg"
+import evenements from "../components/images/evenement.jpg"
+import lieux from "../components/images/lieu.jpeg"
 
+import { Link } from 'react-router-dom';
 
 
 const Accueil = () => {
   return (
-    <div className="index">
-      <div className="div">
-        <div className="overlap-group">
-          <div className="rectangle" />
-          <img
-            className="noah-buscher"
-            alt="Noah buscher"
-            src="https://cdn.animaapp.com/projects/6511c698c032644aea5b6c4d/releases/655633910d79d153e0ed390c/img/noah-buscher-x8zstuks2pm-unsplash-2.png"
-          />
-          <div className="rectangle-2" />
-          <div className="text-wrapper">Parcours</div>
-          <img
-            className="image"
-            alt="Image"
-            src="https://cdn.animaapp.com/projects/6511c698c032644aea5b6c4d/releases/655633910d79d153e0ed390c/img/image-2.png"
-          />
-          <div className="rectangle-3" />
-          <div className="rectangle-4" />
-          <div className="rectangle-4" />
-          <div className="text-wrapper-2">Lieux</div>
-          <img
-            className="tamas-tuzes-katai"
-            alt="Tamas tuzes katai"
-            src="https://cdn.animaapp.com/projects/6511c698c032644aea5b6c4d/releases/655633910d79d153e0ed390c/img/tamas-tuzes-katai-ren-adbr3ig-unsplash-2.png"
-          />
-          <div className="rectangle-5" />
-          <div className="text-wrapper-3">Carte</div>
-          <img
-            className="jakob-dalbjorn"
-            alt="Jakob dalbjorn"
-            src="https://cdn.animaapp.com/projects/6511c698c032644aea5b6c4d/releases/655633910d79d153e0ed390c/img/jakob-dalbjorn-cukjre3nyyc-unsplash-2.png"
-          />
-          <div className="rectangle-6" />
-          <div className="text-wrapper-4">Evenement</div>
-          <img
-            className="img"
-            alt="Image"
-            src="https://cdn.animaapp.com/projects/6511c698c032644aea5b6c4d/releases/655633910d79d153e0ed390c/img/image-4.png"
-          />
-          <p className="p">“Partez à la découverte du Centre Val de Loire”</p>
-          <p className="bienvenue-dans-la">
-            “ Bienvenue dans la magie du Centre-Val de Loire ! Explorez la vallée de la Loire avec ses châteaux
-            emblématiques, plongez dans des vignobles d&#39;exception, et découvrez des paysages naturels à couper le
-            souffle. Notre site vous ouvre les portes d&#39;une expérience unique, mêlant histoire, gastronomie, et
-            charme à la française. Offrez-vous une escapade inoubliable au cœur de cette région captivante. Vivez la
-            France authentique avec le Centre-Val de Loire ! “
-          </p>
-          <div className="text-wrapper-5">A PROPOS</div>
-          <div className="text-wrapper-6">PLAN DU SITE</div>
-          <div className="text-wrapper-7">ACCESSIBILITE</div>
-          <div className="text-wrapper-8">MENTION LEGALES</div>
-          <img
-            className="line"
-            alt="Line"
-            src="https://cdn.animaapp.com/projects/6511c698c032644aea5b6c4d/releases/655633910d79d153e0ed390c/img/line-25.svg"
-          />
-          <img
-            className="line-2"
-            alt="Line"
-            src="https://cdn.animaapp.com/projects/6511c698c032644aea5b6c4d/releases/655633910d79d153e0ed390c/img/line-6.svg"
-          />
-          <img
-            className="line-3"
-            alt="Line"
-            src="https://cdn.animaapp.com/projects/6511c698c032644aea5b6c4d/releases/655633910d79d153e0ed390c/img/line-7.svg"
-          />
-          <img
-            className="line-4"
-            alt="Line"
-            src="https://cdn.animaapp.com/projects/6511c698c032644aea5b6c4d/releases/655633910d79d153e0ed390c/img/line-8.svg"
-          />
-          <img
-            className="image-2"
-            alt="Image"
-            src="https://cdn.animaapp.com/projects/6511c698c032644aea5b6c4d/releases/655633910d79d153e0ed390c/img/image-14.png"
-          />
-        </div>
+    <main>
+      <div className="mt-5 container d-flex justify-content-center align-items-center">
+        <img className="banniereFestival" src={banniereFestival}></img>
       </div>
-    </div>   
+      <div className="container text-center">
+        <h2 className="Titre1 mt-5 d-flex justify-content-center align-items-center">“Partez à la découverte du Centre Val de Loire”</h2>
+        <p className=" mt-2 presentationCVDL">“ Bienvenue dans la magie du Centre-Val de Loire ! Explorez la vallée de la Loire avec ses châteaux emblématiques, plongez dans des vignobles d'exception, et découvrez des paysages naturels à couper le souffle. Notre site vous ouvre les portes d'une expérience unique, mêlant histoire, gastronomie, et charme à la française. Offrez-vous une escapade inoubliable au cœur de cette région captivante. Vivez la France authentique avec le Centre-Val de Loire ! “</p>
+      </div>
+      <div className="d-flex justify-content-center">
+  <div className="d-flex flex-column align-items-center">
+    <div className="m-3">
+      <img className="ImageParcours" src={parcours} alt="Parcours" />
+      <p className="TitreParcours d-flex justify-content-center align-items-center">Parcours</p>
+    </div>
+    <div className="m-3">
+      <img className="ImageLieux" src={lieux} alt="Lieux" />
+      <Link to={`/lieux/`}className="TitreLieux d-flex justify-content-center align-items-center">Lieux</Link>
+    </div>
+  </div>
+  <div className="d-flex flex-column align-items-center">
+    <div className="m-3">
+      <img className="ImageEvenements" src={evenements} alt="Evenements" />
+      <p className="TitreEvenements d-flex justify-content-center align-items-center">Evenements</p>
+    </div>
+    <div className="m-3">
+      <img className="ImageCarte" src={carte} alt="Carte" />
+      <Link to={`/carte/`} className="TitreCarte d-flex justify-content-center align-items-center">Carte</Link>
+    </div>
+  </div>
+</div>
+
+    </main>
   );
 };
 

1437617d1e7e27332972917669083d7e62ac7ab0
FerreiraG <gabriel.ferreira@etu.univ-orleans.fr>
2023-12-02 00:18:14 +0100

    FRONT/IUT-003 mise en page de la carte avec pop up carte

--- artchipelfrontend/src/components/Carte.js
+++ artchipelfrontend/src/components/Carte.js
@@ -1,32 +1,50 @@
 import React, { useEffect, useState } from 'react';
 import { loadModules } from 'esri-loader';
+import "../style/styleCarte.css";
 
 const Carte = () => {
   const [selectedMonument, setSelectedMonument] = useState(null);
-  const [monumentsList, setMonumentsList] = useState([]); // Changement du nom de la variable
+  const [monumentsList, setMonumentsList] = useState([]);
+  let view;
 
   useEffect(() => {
-    loadModules(['esri/Map', 'esri/views/MapView', 'esri/layers/GraphicsLayer', 'esri/Graphic', 'esri/widgets/Directions'], { css: true })
-      .then(([Map, MapView, GraphicsLayer, Graphic, Directions]) => {
+    loadModules(['esri/Map', 'esri/views/MapView', 'esri/layers/GraphicsLayer', 'esri/Graphic', 'esri/widgets/Directions', 'esri/widgets/Popup'], { css: true })
+      .then(([Map, MapView, GraphicsLayer, Graphic, Directions, Popup]) => {
         const map = new Map({
           basemap: 'streets-navigation-vector'
         });
 
-        const view = new MapView({
+        view = new MapView({
           container: 'map-view',
           map: map,
-          center: [1.32, 47.75], // Coordonnées du Centre-Val de Loire
+          center: [1.32, 47.75],
           zoom: 8
         });
 
-        // Ajout de quelques monuments fictifs
         const monumentsLayer = new GraphicsLayer();
         map.add(monumentsLayer);
 
         const monuments = [
           { name: 'Château de Chambord', location: [1.515, 47.616] },
           { name: 'Cathédrale de Chartres', location: [1.485, 48.447] },
-          // Ajoutez d'autres monuments avec leurs coordonnées
+          { name: 'Château de Chenonceau', location: [1.0707, 47.3244] },
+          { name: 'Château d\'Amboise', location: [0.9845, 47.4108] },
+          { name: 'Château de Villandry', location: [0.5110, 47.3423] },
+          { name: 'Château d\'Azay-le-Rideau', location: [0.4725, 47.2613] },
+          { name: 'Château de Blois', location: [1.3307, 47.5861] },
+          { name: 'Château de Cheverny', location: [1.4552, 47.5077] },
+          { name: 'Château de Loches', location: [1.0041, 47.1284] },
+          { name: 'Château de Chinon', location: [0.2454, 47.1676] },
+          { name: 'Château de Montpoupon', location: [0.9833, 47.2167] },
+          { name: 'Château de Chaumont-sur-Loire', location: [0.9760, 47.4826] },
+          { name: 'Château de Valençay', location: [1.5736, 47.1624] },
+          { name: 'Château de Langeais', location: [0.4045, 47.3307] },
+          { name: 'Château de Sully-sur-Loire', location: [2.3832, 47.7803] },
+          { name: 'Château de Gien', location: [2.6340, 47.6876] },
+          { name: 'Château de Meung-sur-Loire', location: [1.6947, 47.8202] },
+          { name: 'Château de La Ferté-Saint-Aubin', location: [1.9583, 47.8151] },
+          { name: 'Château de Saumur', location: [-0.2458, 47.2623] },
+          { name: 'Château de Brissac', location: [-0.5520, 47.3590] },
         ];
 
         monuments.forEach(monument => {
@@ -53,13 +71,27 @@ const Carte = () => {
             }
           });
 
+          const popupTemplate = {
+            title: '{name}',
+            content: 'Détails du monument'
+          };
+
+          pointGraphic.popupTemplate = popupTemplate;
+
           monumentsLayer.add(pointGraphic);
         });
 
-        // Mettez à jour le state des monuments avec la liste des monuments
+        monumentsLayer.on('click', event => {
+          setSelectedMonument(event.graphic.attributes);
+          // Centrer et zoomer sur le monument sélectionné
+          view.goTo({
+            target: event.graphic.geometry,
+            zoom: 12
+          });
+        });
+
         setMonumentsList(monuments);
 
-        // Ajout de la fonctionnalité d'itinéraire
         const directionsWidget = new Directions({
           view: view
         });
@@ -68,33 +100,46 @@ const Carte = () => {
 
         return () => {
           if (view) {
-            // Do any cleanup here
             view.destroy();
           }
         };
       })
       .catch(err => console.error(err));
-  }, []); // La dépendance vide assure que le code ne s'exécute qu'une fois
+  }, []);
+
+  useEffect(() => {
+    if (selectedMonument && view) {
+      // Ouvrir le popup lorsque le monument est sélectionné
+      view.popup.open({
+        title: selectedMonument.name,
+        content: 'Détails du monument'
+      });
+    }
+  }, [selectedMonument]);
 
   return (
-    <div>
-      <div id="map-view" style={{ height: '65vh', width: '100vw' }}></div>
-      <ul className="list-group list-group-flush">
-        {monumentsList.map((monument, index) => (
-          <li key={index} className="list-group-item">
-            <div className="card" style={{ cursor: 'pointer' }} onClick={() => setSelectedMonument(monument)}>
-              <div className="card-body">
-                <h5 className="card-title">{monument.name}</h5>
-                <p className="card-text">
-                  {/* Vous pouvez ajouter d'autres détails du monument ici */}
-                </p>
+    <div className='main'>
+      <div className='d-flex mt-5'>
+        <div id="map-view" style={{ height: '65vh', width: '75vw' }}></div>
+        <h1 className='d-flex titreCarte justify-content-center align-items-center'>Carte</h1>
+      </div>
+      <section className='d-flex'>
+        <h1 className='d-flex titreListeLieux justify-content-center align-items-center'>Liste des monuments</h1>
+        <ul className="list-group d-flex flex-wrap listeLieu">
+          {monumentsList.map((monument, index) => (
+            <li key={index} className="list-group-item" onClick={() => setSelectedMonument(monument)}>
+              <div className="card monument-card border-primary mb-3">
+                <div className="card-body">
+                  <h5 className="card-title text-primary">{monument.name}</h5>
+                  <p className="card-text text-muted">
+                    {/* Vous pouvez ajouter d'autres détails du monument ici */}
+                  </p>
+                </div>
               </div>
-            </div>
-          </li>
-        ))}
-      </ul>
-
-
+            </li>
+          ))}
+        </ul>
+      </section>
     </div>
   );
 };

57bada96364cf5d2888bdf901f77834058bfed7c
FerreiraG <gabriel.ferreira@etu.univ-orleans.fr>
2023-12-10 16:55:28 +0100

    FRONT/IUT-003 footer style et lieu

--- artchipelfrontend/src/components/Footer.js
+++ artchipelfrontend/src/components/Footer.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import '../style/styleFooter.scss';
+import '../style/styleFooter.css';
 
 const Footer = () => {
     return (
         <footer>
-            <p className="">SAE ArtChipel étudiants 3ème année BUT Informatique (Ferreira, Farault, Corret, Kissouna)</p>
-            <p className="">Contact Us : gabriel.ferreira@etu.univ-orleans.fr</p>
+            <div className='d-flex justify-content-center'>
+                <p className="">SAE ArtChipel étudiants 3ème année BUT Informatique (Ferreira, Farault, Corret, Kissouna)</p>
+                <p className="">Contact Us : gabriel.ferreira@etu.univ-orleans.fr</p>
+            </div>
+            <div className='d-flex justify-content-center'>
+                <div className='BleuFooter'></div>
+                <div className='BlancFooter'></div>
+                <div className='RougeFooter'></div>
+            </div>
         </footer>
     );
 };
--- artchipelfrontend/src/components/Lieu.js
+++ artchipelfrontend/src/components/Lieu.js
@@ -9,7 +9,7 @@ const Lieu = () => {
   const [lieu, setLieu] = useState(null);
 
   useEffect(() => {
-    axios.get(`http://localhost:8000/api/Lieu/${id}/`)
+    axios.get(`http://localhost:8000/Lieu/${id}/`)
       .then((response) => {
         const data = response.data;
         console.log(data);
@@ -33,28 +33,54 @@ const Lieu = () => {
             <div className="row">
               <div className="d-flex justify-content-inline">
                 <div className="rectangleBleu1"></div>
-                <h1>{lieu.nomLieu}</h1>
+                <h1>{lieu.lieu.nomLieu}</h1>
               </div>
-              
+
             </div>
             <div className="row">
               <p className="mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt diam nulla, eget laoreet tortor dignissim tincidunt. Proin nec suscipit purus, nec sodales nulla. Fusce dolor lacus, tempor ac cursus a, tempus a justo. Duis posuere augue nec lectus aliquam, ut pulvinar enim rutrum. Donec nulla tortor, viverra ac nunc vel, blandit rutrum sem. Mauris tempor sed tortor at convallis. Cras vitae ligula ultricies, porttitor odio ullamcorper, ultrices elit. In hac habitasse platea dictumst. Nulla nibh sapien, euismod tempus enim ut, volutpat efficitur arcu. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse dapibus turpis mi, vitae congue ex iaculis ut. Quisque a semper mi. Donec sed pellentesque dolor. Nunc at scelerisque nibh. Vivamus ligula dolor, consequat non lectus sit amet, ullamcorper convallis tortor. </p>
             </div>
 
             <div className="row">
-            <div className="d-flex justify-content-end">
-              <div className="rectangleBleu2"></div>
-            </div>
+              <div className="d-flex justify-content-end">
+                <div className="rectangleBleu2"></div>
+              </div>
             </div>
 
           </div>
-          
-                  </div>
+
+        </div>
       </div>
       <div>
-        <h2>Information</h2>
+        <h2 className="titreInformation">Information</h2>
         <div>
-          {/* ajouter les cartes */}
+          <ul className="d-flex list-unstyled ulInformation">
+            <li className="carteVerte">
+              <h4>Tarif Lieu</h4>
+              <p>{lieu.tarif.payant === false ? 'Aucun' : lieu.tarif.payant}</p>
+            </li>
+
+            <li className="carteVerte">
+              <h4>Magasin</h4>
+              <p>{lieu.lieu.boolShopping === false ? 'Indisponible' : 'Disponible'}</p>
+            </li>
+
+            <li className="carteVerte">
+              <h4>Restaurant</h4>
+              <p>{lieu.lieu.boolRepas === false ? 'Indisponible' : 'Disponible'}</p>
+            </li>
+
+            <li className="carteVerte">
+              <h4>Parking</h4>
+              <p>{lieu.lieu.boolParking === false ? 'Indisponible' : 'Disponible'}</p>
+            </li>
+
+            <li className="carteVerte">
+              <h4>Acces Handicapé</h4>
+              <p>{lieu.lieu.boolAccessibilite === false ? 'Indisponible' : 'Disponible'}</p>
+            </li>
+          </ul>
+
         </div>
       </div>
     </div>

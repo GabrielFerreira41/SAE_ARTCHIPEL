1bc97ea935b05175d9fb8e7ffd03ccbe66bf8492
FerreiraG <gabriel.ferreira@etu.univ-orleans.fr>
2023-12-14 12:30:00 +0100

    FRONT/IUT-003 Style Lieux

--- artchipelfrontend/src/components/Lieux.js
+++ artchipelfrontend/src/components/Lieux.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import imageChambord from './images/chambord.jpg';
 import { Link } from "react-router-dom";
 
 import "../style/styleLieux.css";
@@ -21,31 +22,23 @@ const Lieux = () => {
   return (
     <div className="indexAccueil">
       <div className="">
-        <h1 className="titreBlanc p-5">Lieux</h1>
+        <h1 className="d-flex justify-content-center align-items-center titreBlanc p-5">Lieux</h1>
       </div>
 
       <div className="container mt-3 pt-3">
         <div className="row">
           <div className="col-sm-3">
-            <ul className="list-unstyled" style={{ display: 'grid', gridTemplateColumns: 'repeat(5, 1fr)', gap: '10px' }}>
+            <ul className="list-unstyled" style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(350px, 1fr))', gap: '10px' ,width:'70vw'}}>
               {lieux.map((lieu) => (
-                <li key={lieu.idLieu}>
-                  <div className="card mb-3 bg-info" style={{ width: '220px', minHeight: '200px' }}>
-                    <div className="card-body text-white">
-                      <Link
-                        to={{
-                          pathname: `/lieux/${lieu.idLieu}`,
-                          state: { lieuData: lieu.nomLieu }  // Passer le dictionnaire du lieu via le state
-                        }}
-                        className="text-white"
-                      >
-                        {lieu.nomLieu}
-                      </Link>
-                    </div>
+                <li className="" key={lieu.idLieu}>
+                  <div className="m-3">
+                    <img className="ImageCarteLieux" src={imageChambord} alt="Carte" />
+                    <Link to={{ pathname: `/lieux/${lieu.idLieu}` }} className="TitreCarteLieux d-flex justify-content-center align-items-center">{lieu.nomLieu}</Link>
                   </div>
                 </li>
               ))}
             </ul>
+
           </div>
         </div>
       </div>
